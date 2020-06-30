343fabe1f3c2148707969704609fdc4b7635e7f3
Jesse Solis <jesse.solis0316@gmail.com>
2020-06-30 11:16:34 -0500

    add changes for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -27,40 +27,8 @@ app.register_blueprint(users.bp)
 app.register_blueprint(food.bp)
 app.register_blueprint(calorieTracker.bp)
 
-@app.route('/')
-def test():
-    return 'It works'
-
 @app.errorhandler(AuthError)
 def handle_auth_error(ex):
     response = jsonify(ex.error)
     response.status_code = ex.status_code
     return response
-
-# This doesn't need authentication
-@app.route("/api/public")
-@cross_origin(headers=["Content-Type", "Authorization"])
-def public():
-    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
-    return jsonify(message=response)
-
-# This needs authentication
-@app.route("/api/private")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def private():
-    response = "Hello from a private endpoint! You need to be authenticated to see this."
-    return jsonify(message=response)
-
-# This needs authorization
-@app.route("/api/private-scoped")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def private_scoped():
-    if requires_scope("read:messages"):
-        response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
-        return jsonify(message=response)
-    raise AuthError({
-        "code": "Unauthorized",
-        "description": "You don't have access to this resource"
-    }, 403)
