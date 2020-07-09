47934d044ef177d85da7d30578d40ab01c03814f
Jesse Solis <jesse.solis0316@gmail.com>
2020-07-08 22:48:05 -0500

    added validation to post foods

--- app/routes/food.py
+++ app/routes/food.py
@@ -1,8 +1,28 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, g
 from flask_cors import cross_origin
 from ..auth import *
 from ..models import User, db, Food
+from flask_expects_json import expects_json
+
 bp = Blueprint("food", __name__, url_prefix="/food")
+schema = {
+    'type': 'object',
+    'properties': {
+        'user_id': {'type': 'integer'},
+        'total_fat': {'type': 'integer'},
+        'saturated_fat': {'type': 'integer'},
+        'trans_fat': {'type': 'integer'},
+        'cholesterol': {'type': 'integer'},
+        'sodium': {'type': 'integer'},
+        'total_carbs': {'type': 'integer'},
+        'dietary_fiber': {'type': 'integer'},
+        'sugars': {'type': 'integer'},
+        'protein': {'type': 'integer'},
+        'total_cal': {'type': 'integer'},
+        'name': {'type': 'string'}
+    },
+    'required': ['user_id', 'total_fat', 'total_carbs', 'protein', 'total_cal', 'name']
+}
 
 @bp.errorhandler(AuthError)
 def handle_auth_error(ex):
@@ -11,8 +31,10 @@ def handle_auth_error(ex):
     return response
 
 @bp.route('', methods=['POST'])
+@expects_json(schema)
 def postFood():
     body = request.json
+    print(body)
     print(body)
     food = Food(**body)
     db.session.add(food)
