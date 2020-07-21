1c25cf68df47e8a6617c9d32819d3097c9a50d48
Jesse Solis <jesse.solis0316@gmail.com>
2020-07-21 18:08:24 -0500

    added weight form routes

--- app/routes/weightTracker.py
+++ app/routes/weightTracker.py
@@ -30,8 +30,19 @@ def postUserWeight(id):
 def getUserWeight(id):
     body = request.json
     print(body)
-    weights = User_Weight.query.filter_by(user_id=id).all()
+    weights = User_Weight.query.filter_by(user_id=id).order_by(User_Weight.day).all()
 
 
     weight_list = [weight.toDict() for weight in weights]
     return jsonify(weight_list, 200)
+
+@bp.route('/user/<int:id>', methods=['PATCH'])
+def getUserWeightByDay(id):
+    body = request.json
+    date = datetime.datetime(body['day'][0],body['day'][1],body['day'][2])
+    weight = User_Weight.query.filter_by(user_id=id, day=date).first()
+
+    if(weight):
+        return jsonify(weight.toDict(), 200)
+    else:
+        return jsonify('resource not found', 204)
