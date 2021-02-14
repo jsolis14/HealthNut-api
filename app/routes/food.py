a589135c0a83f68daac262f5ec33419297b63551
Jesse Solis <jesse.solis0316@gmail.com>
2021-02-14 10:11:03 -0600

    removed repetitve code from food route

--- app/routes/food.py
+++ app/routes/food.py
@@ -2,28 +2,15 @@ from flask import Blueprint, jsonify, request, g
 from flask_cors import cross_origin
 from ..auth import *
 from ..models import User, db, Food
-from flask_json_schema import JsonSchema, JsonValidationError
 
 bp = Blueprint("food", __name__, url_prefix="/food")
 
-food_schema = {
-    'type': 'object',
-    'properties': {
-        'user_id': {'type': 'integer'},
-        'total_fat': {'type': 'number', 'minimum': 0},
-        'saturated_fat': {'type': 'number', 'minimum': 0},
-        'trans_fat': {'type': 'number', 'minimum': 0},
-        'cholesterol': {'type': 'number', 'minimum': 0},
-        'sodium': {'type': 'number', 'minimum': 0},
-        'total_carbs': {'type': 'number', 'minimum': 0},
-        'dietary_fiber': {'type': 'number', 'minimum': 0},
-        'sugars': {'type': 'number', 'minimum': 0},
-        'protein': {'type': 'number', 'minimum': 0},
-        'total_cal': {'type': 'number', 'minimum': 0},
-        'name': {'type': 'string'}
-    },
-    'required': ['user_id', 'total_fat', 'total_carbs', 'protein', 'total_cal', 'name']
-}
+'''
+---------------------TO DO---------------------------
+- user can enter a string when it should be a number and it wouldn't catch an error
+- lack of requires Auth rapper
+'''
+
 
 @bp.errorhandler(AuthError)
 def handle_auth_error(ex):
@@ -31,8 +18,23 @@ def handle_auth_error(ex):
     response.status_code = ex.status_code
     return response
 
+
 @bp.route('', methods=['POST'])
 def postFood():
+    '''
+    {name: string,
+    total_fat: number,
+    saturated_fat: number,
+    trans_fat: number,
+    cholesterol: number,
+    sodium: number,
+    total_carbs: number,
+    dietary_fiber: number,
+    sugars: number,
+    protein: number,
+    total_cal: number}
+    '''
+
     body = request.json
     errors = []
     name = body['name']
@@ -46,26 +48,21 @@ def postFood():
     sugars = body['sugars']
     protein = body['protein']
     total_cal = body['total_cal']
-    print(body)
+
     if (not name or name == ''):
         errors.append('Please provide a value for name')
-    if ( total_fat == None or total_fat == ''):
+    if (total_fat == None or total_fat == ''):
         errors.append('Please provide a value for total fat')
-    if ( total_carbs == None or total_carbs == ''):
+    if (total_carbs == None or total_carbs == ''):
         errors.append('Please proavide a value for total carbs')
-    if( total_cal == None or total_cal == ''):
+    if(total_cal == None or total_cal == ''):
         errors.append('Please provide a value for total calories')
-    if( protein == None or protein == ''):
+    if(protein == None or protein == ''):
         errors.append('Please provide a value for protein')
-
-    if(len(errors) != 0 ):
-        return jsonify(errors, 400)
-
     if(total_fat < 0 or saturated_fat < 0 or trans_fat < 0 or cholesterol < 0 or sodium < 0 or total_carbs < 0 or dietary_fiber < 0 or sugars < 0 or protein < 0 or total_cal < 0):
         errors.append('values must be greater than 0')
 
-
-    if(len(errors) == 0 ):
+    if(len(errors) == 0):
         food = Food(**body)
         db.session.add(food)
         db.session.commit()
