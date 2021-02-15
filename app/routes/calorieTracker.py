da58ba4b31465a61697c9a0d24277aae456e3584
Jesse Solis <jesse.solis0316@gmail.com>
2021-02-15 12:21:09 -0600

    fixed small bug after adding helper functions

--- app/routes/calorieTracker.py
+++ app/routes/calorieTracker.py
@@ -76,6 +76,13 @@ def getFoods(section, daily_food):
     return foodList
 
 
+def checkIfEmpty(foods):
+    if not foods or len(foods) == 0:
+        return []
+    else:
+        return foods
+
+
 @bp.route('user/<int:id>/foods', methods=['PATCH'])
 @cross_origin(headers=["Content-Type", "Authorization"])
 @requires_auth
@@ -93,60 +100,18 @@ def getCalorieTrackerFoods(id):
 
     # get breakfast
     breakfast_foods_list = getFoods('breakfast_foods', daily_food)
-    breakfast_foods = daily_food.breakfast_foods
+    breakfast_foods = checkIfEmpty(daily_food.breakfast_foods)
 
-    # if(breakfast_foods and len(breakfast_foods) > 0):
-    #     breakfastSet = set(breakfast_foods)
-    #     count = {}
-    #     for food in breakfastSet:
-    #         count[food] = breakfast_foods.count(food)
-    #     for foodId, servings in count.items():
-    #         food = Food.query.get(foodId)
-    #         breakfast_foods_list.append({'food': food.toDict(), 'servings': servings})
-    # else:
-    #     breakfast_foods = []
-    # get lunch
     lunch_foods_list = getFoods('lunch_foods', daily_food)
-    lunch_foods = daily_food.lunch_foods
+    lunch_foods = checkIfEmpty(daily_food.lunch_foods)
 
-    # if(lunch_foods and len(lunch_foods) > 0):
-    #     lunchSet = set(lunch_foods)
-    #     count = {}
-    #     for food in lunchSet:
-    #         count[food] = lunch_foods.count(food)
-    #     for foodId, servings in count.items():
-    #         food = Food.query.get(foodId)
-    #         lunch_foods_list.append({'food': food.toDict(), 'servings': servings})
-    # else:
-    #     lunch_foods = []
-    # get dinner
     dinner_foods_list = getFoods('dinner_foods', daily_food)
-    dinner_foods = daily_food.dinner_foods
+    dinner_foods = checkIfEmpty(daily_food.dinner_foods)
 
-    # if(dinner_foods and len(dinner_foods) > 0):
-    #     dinnerSet = set(dinner_foods)
-    #     count = {}
-    #     for food in dinnerSet:
-    #         count[food] = dinner_foods.count(food)
-    #     for foodId, servings in count.items():
-    #         food = Food.query.get(foodId)
-    #         dinner_foods_list.append({'food': food.toDict(), 'servings': servings})
-    # else:
-    #     dinner_foods=[]
-    # get snack
     snack_foods_list = getFoods('snack_foods', daily_food)
-    snack_foods = daily_food.snack_foods
+    snack_foods = checkIfEmpty(daily_food.snack_foods)
 
-    # if(snack_foods and len(snack_foods) > 0):
-    #     snackSet = set(snack_foods)
-    #     count = {}
-    #     for food in snackSet:
-    #         count[food] = snack_foods.count(food)
-    #     for foodId, servings in count.items():
-    #         food = Food.query.get(foodId)
-    #         snack_foods_list.append({'food': food.toDict(), 'servings': servings})
-    # else:
-    #     snack_foods = []
+    print(breakfast_foods, lunch_foods, dinner_foods, snack_foods)
     return {'breakfast_foods': breakfast_foods_list, 'breakfast_foods_ids': breakfast_foods,
             'lunch_foods': lunch_foods_list, 'lunch_foods_ids': lunch_foods,
             'dinner_foods': dinner_foods_list, 'dinner_foods_ids': dinner_foods,
@@ -193,56 +158,15 @@ def postCalorieTrackerMeal():
 
     if to == 'breakfast':
         addMealId('breakfast_meals', daily_food, db, meal, daily_food_dict)
-        # breakfast_meals = []
-        # if daily_food.breakfast_meals:
-        #     print(daily_food.breakfast_meals)
-        #     breakfast_meals = daily_food_dict['breakfast_meals'].copy()
-        # if meal['id'] in breakfast_meals:
-        #     return jsonify(f"{meal['name']} is arleady in breakfast meals", 400)
-        # breakfast_meals.append(body['meal_id'])
-        # daily_food.breakfast_meals = breakfast_meals
-        # print(breakfast_meals)
-        # db.session.add(daily_food)
-        # db.session.commit()
+
     elif to == 'lunch':
         addMealId('lunch_meals', daily_food, db, meal, daily_food_dict)
-        # lunch_meals = []
-        # if daily_food.lunch_meals:
-        #     print(daily_food.lunch_meals)
-        #     lunch_meals = daily_food_dict['lunch_meals'].copy()
-        # if meal['id'] in lunch_meals:
-        #     return jsonify(f"{meal['name']} is arleady in lunch meals", 400)
-        # lunch_meals.append(body['meal_id'])
-        # daily_food.lunch_meals = lunch_meals
-        # print(lunch_meals)
-        # db.session.add(daily_food)
-        # db.session.commit()
+
     elif to == 'dinner':
         addMealId('dinner_meals', daily_food, db, meal, daily_food_dict)
-        # dinner_meals = []
-        # if daily_food.dinner_meals:
-        #     print(daily_food.dinner_meals)
-        #     dinner_meals = daily_food_dict['dinner_meals'].copy()
-        # if meal['id'] in dinner_meals:
-        #     return jsonify(f"{meal['name']} is arleady in dinner meals", 400)
-        # dinner_meals.append(body['meal_id'])
-        # daily_food.dinner_meals = dinner_meals
-        # print(dinner_meals)
-        # db.session.add(daily_food)
-        # db.session.commit()
+
     elif to == 'snack':
         addMealId('snack_meals', daily_food, db, meal, daily_food_dict)
-        # snack_meals = []
-        # if daily_food.snack_meals:
-        #     print(daily_food.snack_meals)
-        #     snack_meals = daily_food_dict['snack_meals'].copy()
-        # if meal['id'] in snack_meals:
-        #     return jsonify(f"{meal['name']} is arleady in snack meals", 400)
-        # snack_meals.append(body['meal_id'])
-        # daily_food.snack_meals = snack_meals
-        # print(snack_meals)
-        # db.session.add(daily_food)
-        # db.session.commit()
 
     food_ids = meal['food_ids']
     food_ids_set = set(food_ids)
@@ -311,95 +235,14 @@ def getCalorieTrackerMeals(id):
 
     if daily_food.breakfast_meals:
         meal_res['breakfast_meals'] = getMeals(daily_food, 'breakfast_meals')
-        # for meal_id in daily_food.breakfast_meals:
-        #     meal = Meal.query.get(meal_id)
-        #     food_ids = meal.food_ids
-        #     meal = meal.toDict()
-
-        #     food_ids_set = set(food_ids)
-        #     count = {}
-        #     meal_item = {'id': meal['id'], 'name': meal['name'], 'foods': [
-        #     ], 'total_cal': 0, 'total_carbs': 0, 'total_fat': 0, 'total_protein': 0, 'food_ids': meal['food_ids']}
-        #     for food in food_ids_set:
-        #         count[food] = food_ids.count(food)
-        #     for foodId, servings in count.items():
-        #         final_food = Food.query.get(foodId)
-        #         final_food = final_food.toDict()
-        #         meal_item['total_cal'] += final_food['total_cal']*servings
-        #         meal_item['total_carbs'] += final_food['total_carbs']*servings
-        #         meal_item['total_fat'] += final_food['total_fat']*servings
-        #         meal_item['total_protein'] += final_food['protein']*servings
-        #         meal_item['foods'].append({**final_food, 'servings': servings})
-        #     meal_res['breakfast_meals'].append(meal_item)
 
     if daily_food.lunch_meals:
         meal_res['lunch_meals'] = getMeals(daily_food, 'lunch_meals')
-        # for meal_id in daily_food.lunch_meals:
-        #     meal = Meal.query.get(meal_id)
-        #     food_ids = meal.food_ids
-        #     meal = meal.toDict()
-
-        #     food_ids_set = set(food_ids)
-        #     count = {}
-        #     meal_item = {'id': meal['id'], 'name': meal['name'], 'foods': [
-        #     ], 'total_cal': 0, 'total_carbs': 0, 'total_fat': 0, 'total_protein': 0, 'food_ids': meal['food_ids']}
-        #     for food in food_ids_set:
-        #         count[food] = food_ids.count(food)
-        #     for foodId, servings in count.items():
-        #         final_food = Food.query.get(foodId)
-        #         final_food = final_food.toDict()
-        #         meal_item['total_cal'] += final_food['total_cal']*servings
-        #         meal_item['total_carbs'] += final_food['total_carbs']*servings
-        #         meal_item['total_fat'] += final_food['total_fat']*servings
-        #         meal_item['total_protein'] += final_food['protein']*servings
-        #         meal_item['foods'].append({**final_food, 'servings': servings})
-        #     meal_res['lunch_meals'].append(meal_item)
 
     if daily_food.dinner_meals:
-
         meal_res['dinner_meals'] = getMeals(daily_food, 'dinner_meals')
-        # for meal_id in daily_food.dinner_meals:
-        #     meal = Meal.query.get(meal_id)
-        #     food_ids = meal.food_ids
-        #     meal = meal.toDict()
-
-        #     food_ids_set = set(food_ids)
-        #     count = {}
-        #     meal_item = {'id': meal['id'], 'name': meal['name'], 'foods': [
-        #     ], 'total_cal': 0, 'total_carbs': 0, 'total_fat': 0, 'total_protein': 0, 'food_ids': meal['food_ids']}
-        #     for food in food_ids_set:
-        #         count[food] = food_ids.count(food)
-        #     for foodId, servings in count.items():
-        #         final_food = Food.query.get(foodId)
-        #         final_food = final_food.toDict()
-        #         meal_item['total_cal'] += final_food['total_cal']*servings
-        #         meal_item['total_carbs'] += final_food['total_carbs']*servings
-        #         meal_item['total_fat'] += final_food['total_fat']*servings
-        #         meal_item['total_protein'] += final_food['protein']*servings
-        #         meal_item['foods'].append({**final_food, 'servings': servings})
-        #     meal_res['dinner_meals'].append(meal_item)
 
     if daily_food.snack_meals:
         meal_res['snack_meals'] = getMeals(daily_food, 'snack_meals')
-        # for meal_id in daily_food.snack_meals:
-        #     meal = Meal.query.get(meal_id)
-        #     food_ids = meal.food_ids
-        #     meal = meal.toDict()
-
-        #     food_ids_set = set(food_ids)
-        #     count = {}
-        #     meal_item = {'id': meal['id'], 'name': meal['name'], 'foods': [
-        #     ], 'total_cal': 0, 'total_carbs': 0, 'total_fat': 0, 'total_protein': 0, 'food_ids': meal['food_ids']}
-        #     for food in food_ids_set:
-        #         count[food] = food_ids.count(food)
-        #     for foodId, servings in count.items():
-        #         final_food = Food.query.get(foodId)
-        #         final_food = final_food.toDict()
-        #         meal_item['total_cal'] += final_food['total_cal']*servings
-        #         meal_item['total_carbs'] += final_food['total_carbs']*servings
-        #         meal_item['total_fat'] += final_food['total_fat']*servings
-        #         meal_item['total_protein'] += final_food['protein']*servings
-        #         meal_item['foods'].append({**final_food, 'servings': servings})
-        #     meal_res['snack_meals'].append(meal_item)
 
     return jsonify(meal_res, 200)
